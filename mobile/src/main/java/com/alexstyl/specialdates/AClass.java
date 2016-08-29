f7cc738e6c746bc8ce45f5f8f9c1b62707b37618
Alex Styl <alexanderstyl@gmail.com>
2016-08-29 18:55:12 +0100

    Add logging to the random function

--- mobile/src/main/java/com/alexstyl/specialdates/AClass.java
+++ mobile/src/main/java/com/alexstyl/specialdates/AClass.java
@@ -1,8 +1,11 @@
 package com.alexstyl.specialdates;
 
+import com.novoda.notils.logger.simple.Log;
+
 public class AClass {
 
     void someRandomFuncton() {
+        Log.d("Random function called");
         throw new UnsupportedOperationException("This method is not supposed to be called");
     }
 }
