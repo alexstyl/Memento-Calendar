d25ccbd0d5f6b4ed40ae9e7e8ff11133d34a4189
Alex Styl <alexanderstyl@gmail.com>
2016-09-03 23:21:30 +0100

    Add test around bank holidays and namedays

--- mobile/src/test/java/com/alexstyl/specialdates/upcoming/UpcomingDatesBuilderTest.java
+++ mobile/src/test/java/com/alexstyl/specialdates/upcoming/UpcomingDatesBuilderTest.java
@@ -1,11 +1,15 @@
 package com.alexstyl.specialdates.upcoming;
 
+import android.support.annotation.NonNull;
+
 import com.alexstyl.specialdates.DisplayName;
 import com.alexstyl.specialdates.date.CelebrationDate;
 import com.alexstyl.specialdates.date.ContactEvent;
 import com.alexstyl.specialdates.date.DayDate;
 import com.alexstyl.specialdates.events.EventType;
 import com.alexstyl.specialdates.TestContact;
+import com.alexstyl.specialdates.events.bankholidays.BankHoliday;
+import com.alexstyl.specialdates.events.namedays.NamesInADate;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -22,6 +26,7 @@ public class UpcomingDatesBuilderTest {
 
     private static final DayDate FEBRUARY_1st = DayDate.newInstance(1, DayDate.FEBRUARY, 1990);
     private static final DayDate FEBRUARY_3rd = DayDate.newInstance(3, DayDate.FEBRUARY, 1990);
+    private static final DayDate MARCH_5th = DayDate.newInstance(5, DayDate.MARCH, 1990);
 
     @Test
     public void givenASingleContactEvent_thenOneCelebrationDateIsCreated() {
@@ -47,7 +52,7 @@ public class UpcomingDatesBuilderTest {
     }
 
     @Test
-    public void givenTwoContactEventsOnSameDate_thenOneCelebrationDateIsCreated() throws Exception {
+    public void givenTwoContactEventsOnSameDate_thenOneCelebrationDateIsCreated() {
         List<ContactEvent> contactEventsList = Arrays.asList(
                 aContactEventOn(FEBRUARY_1st),
                 aContactEventOn(FEBRUARY_1st)
@@ -61,7 +66,7 @@ public class UpcomingDatesBuilderTest {
     }
 
     @Test
-    public void givenTwoContactEventsOnDifferentDate_thenTwoCelebrationDateAreCreated() throws Exception {
+    public void givenTwoContactEventsOnDifferentDate_thenTwoCelebrationDateAreCreated() {
         List<ContactEvent> contactEventsList = Arrays.asList(
                 aContactEventOn(FEBRUARY_1st),
                 aContactEventOn(FEBRUARY_3rd)
@@ -72,6 +77,36 @@ public class UpcomingDatesBuilderTest {
                 .build();
 
         assertThat(dates.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void givenABankHoliday_thenACelebrationDateIsCreated() {
+        List<BankHoliday> bankHolidays = Collections.singletonList(aBankHoliday());
+        List<CelebrationDate> dates = new UpcomingDatesBuilder()
+                .withBankHolidays(bankHolidays)
+                .build();
+
+        assertThat(dates.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void givenEventsOnDifferentEvents_thenACelebrationDatesForEachOneAreCreated() {
+        List<CelebrationDate> dates = new UpcomingDatesBuilder()
+                .withContactEvents(Collections.singletonList(aContactEventOn(FEBRUARY_1st)))
+                .withBankHolidays(Collections.singletonList(aBankHolidayOn(FEBRUARY_3rd)))
+                .withNamedays(Collections.singletonList(new NamesInADate(MARCH_5th, Collections.singletonList("Name"))))
+                .build();
+
+        assertThat(dates.size()).isEqualTo(3);
+    }
+
+    private BankHoliday aBankHolidayOn(DayDate date) {
+        return new BankHoliday("A bank holiday", date);
+    }
+
+    @NonNull
+    private BankHoliday aBankHoliday() {
+        return new BankHoliday("A bank holiday", DayDate.newInstance(1, 1, 1990));
     }
 
     private static ContactEvent aContactEvent() {
