1acfc7871fcd2e788219c4a3d500635d71995a98
Alex Styl <alexanderstyl@gmail.com>
2016-09-04 12:21:21 +0100

    Add tests around ShortDate

--- mobile/src/main/java/com/alexstyl/specialdates/contact/ShortDate.java
+++ mobile/src/main/java/com/alexstyl/specialdates/contact/ShortDate.java
@@ -5,7 +5,7 @@ public interface ShortDate {
     /**
      * A string representation of the event as it is going to be used for Database transaction purposes.
      * </br> A ShortDate representation of the date "December 29, 1990" would be <i>1990-12-29</i> or <i>--12-29</i> if it is an annual event
-     * (or in case of a birthday the year is unknown).
+     * (such as a birthday.
      * <p>If the event is an annual event, then the year <b>must</b> be ignored</p>
      */
     String toShortDate();
