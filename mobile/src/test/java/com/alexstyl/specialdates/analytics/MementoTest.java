9a7457b3949770496b9d155e1117177e533cab6f
Alex Styl <alexanderstyl@gmail.com>
2016-09-07 21:20:14 +0100

    Write tests around Firebase rules

---
+++ mobile/src/test/java/com/alexstyl/specialdates/analytics/MementoTest.java
@@ -0,0 +1,8 @@
+package com.alexstyl.specialdates.analytics;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MementoTest {
+}
