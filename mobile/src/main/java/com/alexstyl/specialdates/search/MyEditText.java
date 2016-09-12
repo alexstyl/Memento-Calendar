bb4db7d5507d3d05c16feba8a5e3118c57404da9
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2016-09-12 23:38:48 +0100

    Finish Search when the back key is pressed with no search Query

---
+++ mobile/src/main/java/com/alexstyl/specialdates/search/MyEditText.java
@@ -0,0 +1,26 @@
+package com.alexstyl.specialdates.search;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.widget.EditText;
+
+public class MyEditText extends EditText {
+    public MyEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    private OnBackKeyPressedListener listener;
+
+    public void setOnBackKeyPressedListener(OnBackKeyPressedListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public boolean onKeyPreIme(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && listener.onBackButtonPressed()) {
+            return true;
+        }
+        return super.onKeyPreIme(keyCode, event);
+    }
+}
---
+++ mobile/src/main/java/com/alexstyl/specialdates/search/OnBackKeyPressedListener.java
@@ -0,0 +1,8 @@
+package com.alexstyl.specialdates.search;
+
+public interface OnBackKeyPressedListener {
+    /**
+     * @return Return true if the event has been handled
+     */
+    boolean onBackButtonPressed();
+}
