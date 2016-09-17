8f6280eeba1453387cc36b8e3a33933398721b35
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2016-09-18 00:20:53 +0100

    Fix tests

--- mobile/src/test/java/com/alexstyl/specialdates/upcoming/ContactPermissionRequestTest.java
+++ mobile/src/test/java/com/alexstyl/specialdates/upcoming/ContactPermissionRequestTest.java
@@ -1,11 +1,11 @@
 package com.alexstyl.specialdates.upcoming;
 
 import android.app.Activity;
-import android.content.Context;
 
 import com.alexstyl.specialdates.Navigator;
 import com.alexstyl.specialdates.permissions.ContactPermissionRequest;
 import com.alexstyl.specialdates.permissions.ContactPermissionRequest.PermissionCallbacks;
+import com.alexstyl.specialdates.permissions.PermissionChecker;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,24 +20,25 @@ import static org.mockito.Mockito.verify;
 public class ContactPermissionRequestTest {
 
     private static final int CODE = ContactPermissionRequest.CONTACT_REQUEST;
-    @Mock
-    private Context mockContext;
+
     @Mock
     private Navigator mockNavigator;
     @Mock
     private PermissionCallbacks mockCallback;
+    @Mock
+    private PermissionChecker mockChecker;
+
     private ContactPermissionRequest permissions;
 
     @Before
     public void setUp() {
-        permissions = new ContactPermissionRequest(mockNavigator, checker, mockCallback);
+        permissions = new ContactPermissionRequest(mockNavigator, mockChecker, mockCallback);
         Mockito.doNothing().when(mockNavigator).toContactPermissionRequired(CODE);
     }
 
     @Test
     public void whenPermissionIsGrantedCallbackIsCalled() {
         permissions.onActivityResult(CODE, Activity.RESULT_OK, null);
-
         verify(mockCallback).onPermissionGranted();
     }
 
