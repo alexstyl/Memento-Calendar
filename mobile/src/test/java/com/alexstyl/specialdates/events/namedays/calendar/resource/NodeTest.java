8bd5fa7a12715ae05aad9f5a5fd67051a6c43ab7
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2016-09-22 00:23:11 +0100

    Make more tests pass

--- mobile/src/test/java/com/alexstyl/specialdates/events/namedays/calendar/resource/NodeTest.java
+++ mobile/src/test/java/com/alexstyl/specialdates/events/namedays/calendar/resource/NodeTest.java
@@ -6,7 +6,6 @@ import com.alexstyl.specialdates.events.namedays.NameCelebrations;
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
-import static org.fest.assertions.api.Assertions.fail;
 
 public abstract class NodeTest {
 
@@ -18,9 +17,8 @@ public abstract class NodeTest {
         DayDate date = DayDate.newInstance(1, 2, 1990);
         node.addDate("Alex", date);
 
-//        NameCelebrations extracted = node.getDates("Alex");
-//        assertThat(extracted.getDate(0)).isEqualTo(date);
-        fail("Not yet implemented");
+        NameCelebrations extracted = node.getDates("Alex");
+        assertThat(extracted.getDate(0)).isEqualTo(date);
     }
 
     @Test
--- mobile/src/test/java/com/alexstyl/specialdates/util/DateParserTest.java
+++ mobile/src/test/java/com/alexstyl/specialdates/util/DateParserTest.java
@@ -2,7 +2,7 @@ package com.alexstyl.specialdates.util;
 
 import com.alexstyl.specialdates.date.Date;
 import com.alexstyl.specialdates.date.DateParseException;
-import com.alexstyl.specialdates.date.DayDate;
+import com.alexstyl.specialdates.date.ParsedDate;
 
 import org.junit.Test;
 
@@ -16,45 +16,25 @@ public class DateParserTest {
     public void canParseDatesWithDashes() throws DateParseException {
         String dateDashes = "13/Jan/1972";
         Date parsed = dateParser.parse(dateDashes);
-        assertThat(parsed).isEqualTo(DayDate.newInstance(13, 1, 1972));
+        assertThat(parsed).isEqualTo(new ParsedDate(13, 1, 1972));
     }
 
     @Test
     public void canParseLongDates() throws DateParseException {
         String dateDashes = "1949-02-14T00:00:00Z";
         Date parsed = dateParser.parse(dateDashes);
-        assertThat(parsed).isEqualTo(DayDate.newInstance(14, 2, 1949));
+        assertThat(parsed).isEqualTo(new ParsedDate(14, 2, 1949));
     }
 
     @Test
     public void canParseLongDates2() throws DateParseException {
         String dateDashes = "20151026T083936Z";
         Date parsed = dateParser.parse(dateDashes);
-        assertThat(parsed).isEqualTo(DayDate.newInstance(26, 10, 2015));
+        assertThat(parsed).isEqualTo(new ParsedDate(26, 10, 2015));
     }
 
     @Test(expected = DateParseException.class)
     public void throwsExceptionWhenNullIsPassed() throws DateParseException {
         dateParser.parse(null);
-    }
-
-//    "yyyy-MM-dd","--MM-dd",
-//            "MMM dd, yyyy","MMM dd yyyy","MMM dd",
-//
-//            "dd MMM yyyy","dd MMM",// 19 Aug 1990
-//            "yyyyMMdd", // 20110505
-//            "dd MMM yyyy",
-//            "d MMM yyyy", // >>> 6 Δεκ 1980
-//            "dd/MM/yyyy", //22/04/23
-//            "yyyy-MM-dd HH:mm:ss.SSSZ", //ISO 8601
-//            "yyyy-MM-dd'T'HH:mm:ss.SSSZ",
-//            "dd-MM-yyyy", //25-4-1950
-//            "dd/MMMM/yyyy", // 13/Gen/1972
-//            "yyyy-MM-dd'T'HH:mm:ssZ", // 1949-02-14T00:00:00Z
-//            "yyyyMMdd'T'HHmmssZ", // 20151026T083936Z
-
-    @Test
-    public void name() {
-
     }
 }
