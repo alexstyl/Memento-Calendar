5f80ea6b2b6f45aea47796b7de0da6999bdf2edf
Alex Styl <alexanderstyl@gmail.com>
2016-10-22 12:22:46 +0100

    Handle ActivityNotFoundException when launching contact info

--- mobile/src/main/java/com/alexstyl/specialdates/contact/DeviceContact.java
+++ mobile/src/main/java/com/alexstyl/specialdates/contact/DeviceContact.java
@@ -1,5 +1,6 @@
 package com.alexstyl.specialdates.contact;
 
+import android.content.ActivityNotFoundException;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Context;
@@ -8,9 +9,11 @@ import android.net.Uri;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
 import android.view.View;
+import android.widget.Toast;
 
 import com.alexstyl.specialdates.DisplayName;
 import com.alexstyl.specialdates.Optional;
+import com.alexstyl.specialdates.R;
 import com.alexstyl.specialdates.contact.actions.ContactActionFactory;
 import com.alexstyl.specialdates.contact.actions.LabeledAction;
 import com.alexstyl.specialdates.date.Date;
@@ -43,15 +46,18 @@ public class DeviceContact extends Contact {
      * Displays the information window for this contact.
      * </p>
      *
-     * @param context
-     * @param view    The view that was clicked in order to display the winod
+     * @param view The view that was clicked in order to display the winod
      */
     @Override
     public void displayQuickInfo(Context context, View view) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, String.valueOf(contactID));
-        intent.setData(uri);
-        context.startActivity(intent);
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_URI, String.valueOf(contactID));
+            intent.setData(uri);
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException ex) {
+            Toast.makeText(context, R.string.no_app_found, Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
