4f18be62bf0508f5a6db2bf88b5b2a43833c99a6
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2016-11-02 23:20:16 +0000

    Rename Section to EventSection

--- mobile/src/main/java/com/alexstyl/specialdates/ui/EventSection.java
+++ mobile/src/main/java/com/alexstyl/specialdates/ui/EventSection.java
@@ -7,8 +7,7 @@ import com.alexstyl.specialdates.R;
 import java.util.HashMap;
 import java.util.Map;
 
-public enum Section {
-
+public enum EventSection {
     CONTACTS(0, R.drawable.ic_contacts, R.drawable.ic_contacts_disabled),
     BANKHOLIDAYS(1, R.drawable.ic_bankholidays, R.drawable.ic_bankholidays_disabled),
     NAMEDAYS(2, R.drawable.ic_namedays, R.drawable.ic_namedays_disabled);
@@ -19,22 +18,22 @@ public enum Section {
     @DrawableRes
     private final int disabledResId;
 
-    Section(int id, @DrawableRes int enabledResId, @DrawableRes int disabledResId) {
+    EventSection(int id, @DrawableRes int enabledResId, @DrawableRes int disabledResId) {
         this.id = id;
         this.enabledResId = enabledResId;
         this.disabledResId = disabledResId;
     }
 
-    private static final Map<Integer, Section> mapping;
+    private static final Map<Integer, EventSection> mapping;
 
     static {
         mapping = new HashMap<>(values().length);
-        for (Section section : values()) {
+        for (EventSection section : values()) {
             mapping.put(section.id, section);
         }
     }
 
-    public static Section ofId(int position) {
+    public static EventSection ofId(int position) {
         return mapping.get(position);
     }
 
