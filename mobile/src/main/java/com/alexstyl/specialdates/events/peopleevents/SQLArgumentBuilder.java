7b19d3e4f251abb181f55d39060de51dee5af0f4
Alex Styl <alexanderstyl@gmail.com>
2016-11-10 01:16:48 +0000

    Make ContentProvider return the real date of event instead of the year it was asked for

--- mobile/src/main/java/com/alexstyl/specialdates/events/peopleevents/SQLArgumentBuilder.java
+++ mobile/src/main/java/com/alexstyl/specialdates/events/peopleevents/SQLArgumentBuilder.java
@@ -1,29 +1,40 @@
 package com.alexstyl.specialdates.events.peopleevents;
 
 import com.alexstyl.specialdates.date.Date;
-import com.alexstyl.specialdates.events.database.EventsDBContract.AnnualEventsContract;
 import com.alexstyl.specialdates.events.database.PeopleEventsContract;
 import com.alexstyl.specialdates.upcoming.LoadingTimeDuration;
 
 import java.util.Locale;
 
-class SQLArgumentBuilder {
+final class SQLArgumentBuilder {
 
-    private static final String date = PeopleEventsContract.PeopleEvents.DATE;
+    private static final String DATE = PeopleEventsContract.PeopleEvents.DATE;
 
-    String dateIn(int year) {
-        return String.format(Locale.US, "'%d' || substr(date, -6) as '%s'", year, AnnualEventsContract.DATE);
-    }
-
+    // TODO Write tests for this
     String dateBetween(LoadingTimeDuration duration) {
         return dateFrom(duration.getFrom()) + " AND " + dateTo(duration.getTo());
     }
 
-    private String dateFrom(Date date) {
-        return String.format(Locale.US, "'%d' || substr(%s,-6) >= '%s'", date.getYear(), SQLArgumentBuilder.date, date.toString());
+    private static String dateFrom(Date date) {
+        return String.format(Locale.US, "substr(%s,-5) >= '%s'", SQLArgumentBuilder.DATE, dateWithoutYear(date));
     }
 
-    private String dateTo(Date date) {
-        return String.format(Locale.US, "'%d' || substr(%s,-6) <= '%s'", date.getYear(), SQLArgumentBuilder.date, date.toString());
+    private static String dateTo(Date date) {
+        return String.format(Locale.US, "substr(%s,-5) <= '%s'", SQLArgumentBuilder.DATE, dateWithoutYear(date));
+    }
+
+    private static String dateWithoutYear(Date date) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int month = date.getMonth();
+        addWithLeadingZeroIfNeeded(stringBuilder, month);
+        addWithLeadingZeroIfNeeded(stringBuilder, date.getDayOfMonth());
+        return stringBuilder.toString();
+    }
+
+    private static void addWithLeadingZeroIfNeeded(StringBuilder stringBuilder, int value) {
+        if (value < 10) {
+            stringBuilder.append("0");
+        }
+        stringBuilder.append(value);
     }
 }
