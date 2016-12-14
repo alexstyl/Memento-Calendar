795b729f978961125eeaa5b3984836a0384c6898
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2016-12-14 19:35:10 +0000

    Change image paths to Uri

--- mobile/src/main/java/com/alexstyl/specialdates/search/ContactEventViewModel.java
+++ mobile/src/main/java/com/alexstyl/specialdates/search/ContactEventViewModel.java
@@ -1,4 +1,37 @@
 package com.alexstyl.specialdates.search;
 
-public class ContactEventViewModel {
+import android.net.Uri;
+
+import com.alexstyl.specialdates.contact.Contact;
+
+class ContactEventViewModel {
+    private final Contact contact;
+    private final String eventLabel;
+    private final int backgroundVariant;
+
+    ContactEventViewModel(Contact contact, String eventLabel, int backgroundVariant) {
+        this.contact = contact;
+        this.eventLabel = eventLabel;
+        this.backgroundVariant = backgroundVariant;
+    }
+
+    int getBackgroundVariant() {
+        return backgroundVariant;
+    }
+
+    String getDisplayName() {
+        return contact.getDisplayName().toString();
+    }
+
+    Uri getContactAvatarURI() {
+        return contact.getImagePath();
+    }
+
+    String getEventLabel() {
+        return eventLabel;
+    }
+
+    Contact getContact() {
+        return contact;
+    }
 }
