7e4dae21e00960653604c88937bce292524b945c
Alex Styl <alexanderstyl@gmail.com>
2016-12-28 15:51:31 +0200

    Grant read permission for pre lollipop

--- mobile/src/main/java/com/alexstyl/specialdates/addevent/ImagePickIntent.java
+++
@@ -1,9 +0,0 @@
-package com.alexstyl.specialdates.addevent;
-
-import android.content.Intent;
-import android.net.Uri;
-
-interface ImagePickIntent {
-
-    Intent createIntentWithOutput(Uri imagePath);
-}
