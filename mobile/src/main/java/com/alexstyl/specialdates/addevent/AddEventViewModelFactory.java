5a078d09b490d2a99c9e342f36e5ffa728fcb6f3
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2017-01-14 22:40:48 +0200

    Remove creation of custom events

--- mobile/src/main/java/com/alexstyl/specialdates/addevent/AddEventViewModelFactory.java
+++ mobile/src/main/java/com/alexstyl/specialdates/addevent/AddEventViewModelFactory.java
@@ -5,7 +5,6 @@ import android.view.View;
 import com.alexstyl.resources.StringResources;
 import com.alexstyl.specialdates.Optional;
 import com.alexstyl.specialdates.date.Date;
-import com.alexstyl.specialdates.events.peopleevents.CustomEventType;
 import com.alexstyl.specialdates.events.peopleevents.EventType;
 import com.alexstyl.specialdates.events.peopleevents.StandardEventType;
 
@@ -23,13 +22,11 @@ final class AddEventViewModelFactory {
 
     List<ContactEventViewModel> createViewModelsForAllEventsBut(List<EventType> existingTypes) {
         List<ContactEventViewModel> addEventViewModels = new ArrayList<>();
-        boolean containsCustomType = containsCustomType(existingTypes);
 
         for (StandardEventType eventType : StandardEventType.values()) {
             if (existingTypes.contains(eventType)
                     || eventType == StandardEventType.NAMEDAY
-                    || (eventType == StandardEventType.CUSTOM && containsCustomType)
-                    ) {
+                    || eventType == StandardEventType.CUSTOM) {
                 continue;
             }
             addEventViewModels.add(createAddEventViewModelsFor(eventType));
@@ -40,15 +37,6 @@ final class AddEventViewModelFactory {
     ContactEventViewModel createAddEventViewModelsFor(EventType eventType) {
         String eventName = eventType.getEventName(stringResources);
         return new ContactEventViewModel(eventName, eventType, NO_DATE, View.GONE);
-    }
-
-    private boolean containsCustomType(List<EventType> existingTypes) {
-        for (EventType existingType : existingTypes) {
-            if (existingType instanceof CustomEventType) {
-                return true;
-            }
-        }
-        return false;
     }
 
 }
