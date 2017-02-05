c85ce94103ba2cc1a1b1c488e5efe272ad86c7b9
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2017-02-05 15:36:06 +0000

    Dummy way of showing ads

--- mobile/src/main/java/com/alexstyl/specialdates/upcoming/UpcomingRowViewType.java
+++ mobile/src/main/java/com/alexstyl/specialdates/upcoming/UpcomingRowViewType.java
@@ -5,19 +5,19 @@ import android.support.annotation.IntDef;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-import static com.alexstyl.specialdates.upcoming.UpcomingRowViewType.UPCOMING_EVENTS;
-import static com.alexstyl.specialdates.upcoming.UpcomingRowViewType.MONTH;
-import static com.alexstyl.specialdates.upcoming.UpcomingRowViewType.YEAR;
+import static com.alexstyl.specialdates.upcoming.UpcomingRowViewType.*;
 
 @Retention(RetentionPolicy.SOURCE)
 @IntDef({
         MONTH,
         UPCOMING_EVENTS,
-        YEAR
+        YEAR,
+        AD
 })
 public @interface UpcomingRowViewType {
 
     int MONTH = 0;
     int UPCOMING_EVENTS = 1;
     int YEAR = 2;
+    int AD = 3;
 }
--- mobile/src/main/java/com/alexstyl/specialdates/upcoming/view/UpcomingViewHolderFactory.java
+++ mobile/src/main/java/com/alexstyl/specialdates/upcoming/view/UpcomingViewHolderFactory.java
@@ -8,8 +8,13 @@ import android.widget.TextView;
 import com.alexstyl.specialdates.R;
 import com.alexstyl.specialdates.images.ImageLoader;
 import com.alexstyl.specialdates.upcoming.UpcomingRowViewType;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.NativeExpressAdView;
 import com.novoda.notils.caster.Views;
 import com.novoda.notils.exception.DeveloperError;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 final class UpcomingViewHolderFactory {
 
@@ -35,6 +40,12 @@ final class UpcomingViewHolderFactory {
             View view = layoutInflater.inflate(R.layout.row_header, parent, false);
             TextView headerView = (TextView) view.findViewById(R.id.upcoming_event_header);
             return new YearViewHolder(view, headerView);
+        } else if (viewType == UpcomingRowViewType.AD) {
+            NativeExpressAdView adView = new NativeExpressAdView(parent.getContext());
+            adView.setLayoutParams(new ViewGroup.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+            adView.setAdSize(new AdSize(AdSize.FULL_WIDTH, 132));
+            adView.setAdUnitId(parent.getResources().getString(R.string.admob_unit_id));
+            return new AdViewHolder(adView, adView);
         } else {
             throw new DeveloperError("Invalid viewType " + viewType);
         }
