977200ccf872a0d6d2703b65bf64da9a96554d3e
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2017-04-19 22:13:14 +0100

    Remove empty line

--- mobile/src/main/java/com/alexstyl/specialdates/images/OnImageLoadedCallback.java
+++ mobile/src/main/java/com/alexstyl/specialdates/images/OnImageLoadedCallback.java
@@ -3,7 +3,6 @@ package com.alexstyl.specialdates.images;
 import android.graphics.Bitmap;
 
 public interface OnImageLoadedCallback {
-
     void onImageLoaded(Bitmap loadedImage);
 
     void onLoadingFailed();
