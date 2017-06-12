54777e893858e7b6081c5665dd24be805e558cd8
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2017-06-12 20:02:20 +0100

    Use the layoutManager span instead of a static one

--- android_mobile/src/main/java/com/alexstyl/specialdates/datedetails/DateDetailsSpanLookup.java
+++ android_mobile/src/main/java/com/alexstyl/specialdates/datedetails/DateDetailsSpanLookup.java
@@ -3,12 +3,14 @@ package com.alexstyl.specialdates.datedetails;
 import android.support.v7.widget.GridLayoutManager;
 
 final class DateDetailsSpanLookup extends GridLayoutManager.SpanSizeLookup {
-    public static final int FULL_SPAN = 2;
     public static final int HALF_SPAN = 1;
+    public static final int FULL_SPAN = 2;
     private final DateDetailsAdapter adapter;
+    private final GridLayoutManager layoutManager;
 
-    DateDetailsSpanLookup(DateDetailsAdapter adapter) {
+    DateDetailsSpanLookup(DateDetailsAdapter adapter, GridLayoutManager layoutManager) {
         this.adapter = adapter;
+        this.layoutManager = layoutManager;
     }
 
     @Override
@@ -16,7 +18,7 @@ final class DateDetailsSpanLookup extends GridLayoutManager.SpanSizeLookup {
         int viewType = adapter.getItemViewType(position);
         if (viewType == DateDetailsViewType.BANKHOLIDAY ||
                 viewType == DateDetailsViewType.NAMEDAY) {
-            return FULL_SPAN;
+            return layoutManager.getSpanCount();
         }
         return HALF_SPAN;
     }
