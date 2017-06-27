b0e359b5509948f8ae97d2a9a6013675da5e4a55
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2017-06-27 13:10:07 +0100

    Add FacebookProfileActivity

---
+++ android_mobile/src/main/java/com/alexstyl/specialdates/facebook/FacebookProfileActivity.java
@@ -0,0 +1,15 @@
+package com.alexstyl.specialdates.facebook;
+
+import android.os.Bundle;
+
+import com.alexstyl.specialdates.R;
+import com.alexstyl.specialdates.ui.base.ThemedMementoActivity;
+
+public class FacebookProfileActivity extends ThemedMementoActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_profile);
+    }
+}
