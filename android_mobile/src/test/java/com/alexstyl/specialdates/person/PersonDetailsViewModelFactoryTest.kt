4e10d4a2cc014a25489849aef3593325949664c2
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2017-07-17 23:10:41 +0100

    WIP Introduce PersonPersenter.kt

---
+++ android_mobile/src/test/java/com/alexstyl/specialdates/person/PersonDetailsViewModelFactoryTest.kt
@@ -0,0 +1,49 @@
+package com.alexstyl.specialdates.person
+
+import com.alexstyl.specialdates.Optional
+import com.alexstyl.specialdates.contact.ContactFixture
+import com.alexstyl.specialdates.date.ContactEvent
+import com.alexstyl.specialdates.date.Date
+import com.alexstyl.specialdates.date.DateConstants.JANUARY
+import com.alexstyl.specialdates.events.peopleevents.StandardEventType
+import org.fest.assertions.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.runners.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class PersonDetailsViewModelFactoryTest {
+
+    private val factory = PersonDetailsViewModelFactory()
+
+    @Test
+    fun whenPassingAContact_thenAlwaysReturnItsName() {
+        var resultViewModel = factory.apply(ContactFixture.withName("Anna Roberts"), null)
+        assertThat(resultViewModel.displayName).isEqualTo("Anna Roberts")
+    }
+
+    @Test
+    fun whenPassingNoContactEvent_thenAgeAndStarSignIsEmptyString() {
+        var resultViewModel = factory.apply(ContactFixture.withName("Anna Roberts"), null)
+        assertThat(resultViewModel.ageAndStarSignlabel).isEqualTo("")
+    }
+
+    @Test
+    fun whenPassingABirthdayWithOutYear_thenAgeAndStarSignContainsOnlyStarSign() {
+        val dateOfBirth = Date.on(1, JANUARY)
+        val contactEvent = ContactEvent(Optional.absent(), StandardEventType.BIRTHDAY, dateOfBirth, ContactFixture.withName("Anna Roberts"))
+
+        var resultViewModel = factory.apply(ContactFixture.withName("Anna Roberts"), contactEvent)
+        assertThat(resultViewModel.ageAndStarSignlabel).isEqualTo("Sagittarius")
+    }
+
+    @Test
+    fun whenPassingABirthdayWithYear_thenAgeAndStarSignContainsBothAgeAndStarSign() {
+        val dateOfBirth = Date.on(1, JANUARY, 1990)
+        val contactEvent = ContactEvent(Optional.absent(), StandardEventType.BIRTHDAY, dateOfBirth, ContactFixture.withName("Anna Roberts"))
+
+        var resultViewModel = factory.apply(ContactFixture.withName("Anna Roberts"), contactEvent)
+        assertThat(resultViewModel.ageAndStarSignlabel).isEqualTo("26, Sagittarius")
+    }
+
+}
