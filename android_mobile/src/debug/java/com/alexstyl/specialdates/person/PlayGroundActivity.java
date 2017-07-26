888ee56da632265b9b45ea71ea3aa79702900d13
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2017-07-26 14:04:55 +0100

    Display icons for custom items

--- android_mobile/src/debug/java/com/alexstyl/specialdates/person/PlayGroundActivity.java
+++ android_mobile/src/debug/java/com/alexstyl/specialdates/person/PlayGroundActivity.java
@@ -2,15 +2,7 @@ package com.alexstyl.specialdates.person;
 
 import android.os.Bundle;
 
-import com.alexstyl.specialdates.android.AndroidStringResources;
-import com.alexstyl.specialdates.contact.Contact;
-import com.alexstyl.specialdates.contact.ContactSource;
-import com.alexstyl.specialdates.contact.DisplayName;
 import com.alexstyl.specialdates.ui.base.ThemedMementoActivity;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 public class PlayGroundActivity extends ThemedMementoActivity {
 
@@ -18,22 +10,23 @@ public class PlayGroundActivity extends ThemedMementoActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        AndroidContactCallActionsProvider actionsProvider = new AndroidContactCallActionsProvider(
-                getContentResolver(),
-                new AndroidStringResources(getResources()),
-                new AndroidContactActionsFactory(thisActivity())
-        );
+        AndroidContactCallActionsProvider actionsProvider = null;
+//        new AndroidContactCallActionsProvider(
+//                getContentResolver(),
+//                new AndroidStringResources(getResources()),
+//                new AndroidContactActionsFactory(thisActivity())
+//        );
 
-        Contact contact = new Contact(250L, DisplayName.NO_NAME, URI.create(""), ContactSource.SOURCE_DEVICE);
-        List<ContactAction> actions = new ArrayList<>();
-        List<ContactAction> callActions = actionsProvider.callActionsFor(contact);
-        actions.addAll(callActions);
-        List<ContactAction> customActions = actionsProvider.customActionsFor(contact);
-        actions.addAll(customActions);
-        for (ContactAction action : actions) {
-            print(action.toString());
-        }
-        print("done");
+//        Contact contact = new Contact(250L, DisplayName.NO_NAME, URI.create(""), ContactSource.SOURCE_DEVICE);
+//        List<ContactAction> actions = new ArrayList<>();
+//        List<ContactAction> callActions = actionsProvider.callActionsFor(contact);
+//        actions.addAll(callActions);
+//        List<ContactAction> customActions = actionsProvider.customActionsFor(contact);
+//        actions.addAll(customActions);
+//        for (ContactAction action : actions) {
+//            print(action.toString());
+//        }
+//        print("done");
 
     }
 
