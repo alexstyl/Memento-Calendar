1c54cb6389ec7d10305aded459dd0f10a19e5264
Alex Styl <alexanderstyl@gmail.com>
2017-08-07 02:44:28 +0100

    Fetch correct special namedays for contacts

--- android_mobile/src/main/java/com/alexstyl/specialdates/events/peopleevents/PeopleNamedaysCalculator.java
+++ android_mobile/src/main/java/com/alexstyl/specialdates/events/peopleevents/PeopleNamedaysCalculator.java
@@ -1,11 +1,12 @@
 package com.alexstyl.specialdates.events.peopleevents;
 
-import com.alexstyl.specialdates.contact.DisplayName;
 import com.alexstyl.specialdates.Optional;
 import com.alexstyl.specialdates.contact.Contact;
 import com.alexstyl.specialdates.contact.ContactsProvider;
+import com.alexstyl.specialdates.contact.DisplayName;
 import com.alexstyl.specialdates.date.ContactEvent;
 import com.alexstyl.specialdates.date.Date;
+import com.alexstyl.specialdates.date.Dates;
 import com.alexstyl.specialdates.date.TimePeriod;
 import com.alexstyl.specialdates.events.namedays.NameCelebrations;
 import com.alexstyl.specialdates.events.namedays.NamedayLocale;
@@ -19,8 +20,6 @@ import java.util.HashSet;
 import java.util.List;
 
 public class PeopleNamedaysCalculator {
-
-    private static final Optional<Long> NO_DEVICE_EVENT_ID = Optional.absent();
 
     private final NamedayPreferences namedayPreferences;
     private final NamedayCalendarProvider namedayCalendarProvider;
@@ -53,7 +52,7 @@ public class PeopleNamedaysCalculator {
                     if (namedays.contains(date)) {
                         continue;
                     }
-                    ContactEvent event = new ContactEvent(NO_DEVICE_EVENT_ID, StandardEventType.NAMEDAY, date, contact);
+                    ContactEvent event = new ContactEvent(new Optional<>(contact.getContactID()), StandardEventType.NAMEDAY, date, contact);
                     namedayEvents.add(event);
                     namedays.add(date);
                 }
@@ -81,7 +80,7 @@ public class PeopleNamedaysCalculator {
                 for (int i = 0; i < namedaysCount; i++) {
                     Date date = nameDays.getDate(i);
                     if (timeDuration.containsDate(date)) {
-                        ContactEvent nameday = new ContactEvent(NO_DEVICE_EVENT_ID, StandardEventType.NAMEDAY, date, contact);
+                        ContactEvent nameday = new ContactEvent(new Optional<>(contact.getContactID()), StandardEventType.NAMEDAY, date, contact);
                         namedayEvents.add(nameday);
                     }
                 }
@@ -90,12 +89,25 @@ public class PeopleNamedaysCalculator {
         return Collections.unmodifiableList(namedayEvents);
     }
 
+    public List<ContactEvent> loadSpecialNamedaysFor(Contact contact) {
+        List<ContactEvent> namedays = new ArrayList<>();
+        for (String name : contact.getDisplayName().getAllNames()) {
+            NameCelebrations specialCelebration = getSpecialNamedaysOf(name);
+            Dates specialDates = specialCelebration.getDates();
+            for (int i = 0; i < specialDates.size(); i++) {
+                ContactEvent contactEvent = new ContactEvent(new Optional<>(contact.getContactID()), StandardEventType.NAMEDAY, specialDates.getDate(i), contact);
+                namedays.add(contactEvent);
+            }
+        }
+        return namedays;
+    }
+
     private NameCelebrations getNamedaysOf(String given) {
         NamedayCalendar namedayCalendar = getNamedayCalendar();
         return namedayCalendar.getNormalNamedaysFor(given);
     }
 
-    private NameCelebrations getSpecialNamedaysOf(String firstName) {
+    NameCelebrations getSpecialNamedaysOf(String firstName) {
         NamedayCalendar namedayCalendar = getNamedayCalendar();
         return namedayCalendar.getSpecialNamedaysFor(firstName);
     }
@@ -103,9 +115,5 @@ public class PeopleNamedaysCalculator {
     private NamedayCalendar getNamedayCalendar() {
         NamedayLocale locale = namedayPreferences.getSelectedLanguage();
         return namedayCalendarProvider.loadNamedayCalendarForLocale(locale, Date.Companion.getCURRENT_YEAR());
-    }
-
-    public List<ContactEvent> loadSpecialNamedaysFor(Contact contact) {
-        throw new UnsupportedOperationException();
     }
 }
