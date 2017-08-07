8e170d43c61b1d1048f9cab0414e079cb50f52ee
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2017-08-07 04:32:22 +0100

    Add tests around UpcomingRowViewModelsBuilder

--- android_mobile/src/main/java/com/alexstyl/specialdates/upcoming/NamedaysViewModel.kt
+++ android_mobile/src/main/java/com/alexstyl/specialdates/upcoming/NamedaysViewModel.kt
@@ -1,6 +1,6 @@
 package com.alexstyl.specialdates.upcoming
 
-internal class NamedaysViewModel(val namesLabel: String) : UpcomingRowViewModel {
+data class NamedaysViewModel(val namesLabel: String) : UpcomingRowViewModel {
 
     override val viewType: Int
         get() = UpcomingRowViewType.NAMEDAY_CARD
