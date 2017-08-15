1982ce583f20ecb2b5d7303b448a66d26c24044c
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2017-08-16 00:32:38 +0100

    Display nameday on launch

--- android_mobile/src/main/java/com/alexstyl/specialdates/events/namedays/activity/NamedaysMVPView.kt
+++ android_mobile/src/main/java/com/alexstyl/specialdates/events/namedays/activity/NamedaysMVPView.kt
@@ -1,7 +1,5 @@
 package com.alexstyl.specialdates.events.namedays.activity
 
-import com.alexstyl.specialdates.date.Date
-
 interface NamedaysMVPView {
-    fun displayNamedays(date: Date, viewModels: List<NamedayScreenViewModel>)
+    fun displayNamedays(viewModels: List<NamedayScreenViewModel>)
 }
