607b39e456fc20644125da1985b824bb5a275b71
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2017-08-22 00:14:17 +0100

    Rewrite SoundNode to kotlin

--- android_mobile/src/main/java/com/alexstyl/specialdates/events/namedays/calendar/resource/SoundNode.kt
+++ android_mobile/src/main/java/com/alexstyl/specialdates/events/namedays/calendar/resource/SoundNode.kt
@@ -1,17 +1,13 @@
 package com.alexstyl.specialdates.events.namedays.calendar.resource
 
-import com.alexstyl.gsc.Index
 import com.alexstyl.gsc.Sound
 import com.alexstyl.gsc.SoundRules
 import com.alexstyl.specialdates.date.Date
-import com.alexstyl.specialdates.date.Dates
 import com.alexstyl.specialdates.events.namedays.NameCelebrations
 import java.util.*
 
 
-class SoundNode private constructor(
-        private val keySound: Sound? // gk
-) : Node {
+class SoundNode private constructor(private val keySound: Sound?) : Node {
     private val nodes = ArrayList<SoundNode>() // a b c d
     private var dates: NameCelebrations? = null // Name
 
@@ -43,68 +39,62 @@ class SoundNode private constructor(
      * Adds the given date for the given word
      */
     override fun addDate(word: String, date: Date) {
-        addDateInternal(Index(word.length), word, date)
+        val nextSounds = SoundRules.INSTANCE.getNextSound(word, false)
+        addDate(word, date, nextSounds)
     }
 
-    private fun addDateInternal(index: Index, word: String, date: Date) {
-        val nextSound = SoundRules.INSTANCE.getNextSound(word, false).take(100)
-
-
-//        val s = if (index.hasEnded()) null else nextSound
-//        if (s == null) {
-//            if (dates == null) {
-//                dates = NameCelebrations(word)
-//            }
-//            dates!!.addDate(date)
-//
-//        } else {
-//            var theNode: SoundNode? = null
-//            for (node in nodes!!) {
-//                if (node.keySound == s) {
-//                    theNode = node
-//                    break
-//                }
-//            }
-//            if (theNode == null) {
-//                theNode = SoundNode(s)
-//                nodes.add(theNode)
-//            }
-//            theNode.addDateInternal(index, word, date)
-//
-//        }
+    fun addDate(word: String, date: Date, iterator: Iterator<Sound?>) {
+        if (!iterator.hasNext()) {
+            // we are done.
+            if (dates == null) {
+                dates = NameCelebrations(word)
+            }
+            dates!!.addDate(date)
+        } else {
+            var theNode: SoundNode? = null
+            iterator.forEach { s ->
+                nodes.forEach { node ->
+                    if (node.keySound == s) {
+                        theNode = node
+                        return@forEach
+                    }
+                }
+                if (theNode == null) {
+                    theNode = SoundNode(s)
+                    nodes.add(theNode!!)
+                }
+                theNode!!.addDate(word, date, iterator)
+            }
+        }
     }
 
-    override fun getDates(name: String): NameCelebrations {
-//        var nextSound = SoundRules.INSTANCE.getNextSound(name, false)
-//
-//
-//        val s = if (index.hasEnded()) null else SoundRules.getInstance().getNextSound(name, index, false)
-//        if (s == null) {
-//            return getDates()
-//        } else {
-//            for (node in nodes) {
-//                if (node.keySound.equals(s)) {
-//                    val currentIndex = index.intValue()
-//                    val e = node.getDatesInternal(index, name)
-//                    if (e != null) {
-//                        return e
-//                    }
-//                    index.setTo(currentIndex)
-//                }
-//            }
-//            return NameCelebrations(name)
-//        }
-        return NO_NAMEDAYS
+    override fun getDates(name: String): NameCelebrations? = getDates(name, SoundRules.INSTANCE.getNextSound(name, false))
+
+    fun getDates(name: String, iterator: Iterator<Sound?>): NameCelebrations? {
+        if (!iterator.hasNext()) {
+            // we are done.
+            return getNameCelebrations(name)
+        } else {
+            iterator.forEach { s ->
+                nodes.forEach { node ->
+                    if (node.keySound == s) {
+                        val nameCelebrations = node.getDates(name, iterator)
+                        if (nameCelebrations != null) {
+                            return nameCelebrations
+                        }
+                    }
+                }
+            }
+            return NameCelebrations(name)
+        }
     }
 
-
-    private fun getDates(): NameCelebrations {
+    private fun getNameCelebrations(name: String): NameCelebrations {
         return if (dates == null) {
-            NO_NAMEDAYS
+            NameCelebrations(name)
         } else {
             dates!!
         }
     }
 
-    private val NO_NAMEDAYS = NameCelebrations("", Dates())
 }
