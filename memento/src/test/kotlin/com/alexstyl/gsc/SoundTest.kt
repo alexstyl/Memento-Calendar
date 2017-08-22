55f1b394fbf1f77706922af8e02950b696965577
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2017-08-22 22:37:09 +0100

    Plus operation and rename combine to flatten

--- memento/src/test/kotlin/com/alexstyl/gsc/SoundTest.kt
+++ memento/src/test/kotlin/com/alexstyl/gsc/SoundTest.kt
@@ -1,6 +1,23 @@
 package com.alexstyl.gsc
 
-import org.junit.Assert.*
+import org.fest.assertions.api.Assertions
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.runners.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
-class SoundTest
+class SoundTest {
+
+    @Test
+    fun combiningTwoSymbols_returnsASoundWithAllTheSymbols() {
+        val result = Sound('A') + Sound('E')
+        Assertions.assertThat(result).isEqualTo(Sound(charArrayOf('A', 'E')))
+    }
+
+
+    @Test
+    fun appendingThreeSounds_returnsASoundWithAllSymbols() {
+        var result = Sound.flatten(listOf(Sound('A'), Sound('B'), Sound('C')))
+        Assertions.assertThat(result).isEqualTo(Sound("A,B,C"))
+    }
+}
