5c45cd916a855d24fc5547037926d09bef964a23
Alex Styl <alexanderstyl@gmail.com>
2017-08-22 23:30:05 +0100

    Fix issue with returning maching sounds

--- android_mobile/src/main/java/com/alexstyl/specialdates/SoundWordComparator.java
+++ android_mobile/src/main/java/com/alexstyl/specialdates/SoundWordComparator.java
@@ -6,7 +6,7 @@ public class SoundWordComparator implements WordComparator {
 
     @Override
     public boolean compare(String aWord, String anOtherWord) {
-        return SoundComparer.Companion.startsWith(aWord, anOtherWord);
+        return SoundComparer.Companion.soundTheSame(aWord, anOtherWord);
     }
 
     @Override
