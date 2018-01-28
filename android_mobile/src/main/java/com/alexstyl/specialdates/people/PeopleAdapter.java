26af87b060e78dd9c12ff30716c59b560a535049
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2018-01-28 16:26:56 +0000

    Add more spacing to the content

--- android_mobile/src/main/java/com/alexstyl/specialdates/people/PeopleAdapter.java
+++ android_mobile/src/main/java/com/alexstyl/specialdates/people/PeopleAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
