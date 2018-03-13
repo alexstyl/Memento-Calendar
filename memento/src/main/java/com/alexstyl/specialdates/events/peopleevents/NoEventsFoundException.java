9348dc5bfaebf8183500345a3be115805b31b527
Alex Styl <alexanderstyl@gmail.com>
2018-03-13 20:42:49 +0000

    Remove java util imports

--- memento/src/main/java/com/alexstyl/specialdates/events/peopleevents/NoEventsFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.alexstyl.specialdates.events.peopleevents;
-
-public final class NoEventsFoundException extends Exception {
-    NoEventsFoundException(String message) {
-        super(message);
-    }
-}
