d1937ee9a2ae3f6ca72fdf8f0ca535f29196e16b
Alex Styl <alexanderstyl@gmail.com>
2018-03-13 20:35:41 +0000

    Remove java list from import

--- memento/src/main/java/com/alexstyl/specialdates/events/peopleevents/ContactEventsOnADate.kt
+++ memento/src/main/java/com/alexstyl/specialdates/events/peopleevents/ContactEventsOnADate.kt
@@ -3,7 +3,6 @@ package com.alexstyl.specialdates.events.peopleevents
 import com.alexstyl.specialdates.contact.Contact
 import com.alexstyl.specialdates.date.ContactEvent
 import com.alexstyl.specialdates.date.Date
-import java.util.*
 
 data class ContactEventsOnADate(val date: Date, val events: List<ContactEvent>, val contacts: List<Contact>) {
 
@@ -14,14 +13,15 @@ data class ContactEventsOnADate(val date: Date, val events: List<ContactEvent>, 
             return ContactEventsOnADate(date, contactEvent, contacts)
         }
 
-        private fun getContactsIn(contactEvent: List<ContactEvent>): List<Contact> {
+        private fun getContactsIn(events: List<ContactEvent>): List<Contact> {
             val contacts = ArrayList<Contact>()
-            for ((_, _, _, contact) in contactEvent) {
+            for ((_, _, _, contact) in events) {
                 if (!contacts.contains(contact)) {
                     contacts.add(contact)
                 }
             }
-            return Collections.unmodifiableList(contacts)
+
+            return contacts.toList()
         }
     }
 }
