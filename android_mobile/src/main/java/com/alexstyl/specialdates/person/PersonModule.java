f9018d051dcd9c8b1afeb2973ceffe80232cdd5a
Alex Styl <alexanderstyl@gmail.com>
2018-03-30 19:11:47 +0100

    Modularise presenter even more

--- android_mobile/src/main/java/com/alexstyl/specialdates/person/PersonModule.java
+++ android_mobile/src/main/java/com/alexstyl/specialdates/person/PersonModule.java
@@ -11,8 +11,6 @@ import com.alexstyl.specialdates.date.Date;
 import com.alexstyl.specialdates.date.DateLabelCreator;
 import com.alexstyl.specialdates.events.peopleevents.PeopleEventsPersister;
 import com.alexstyl.specialdates.events.peopleevents.PeopleEventsProvider;
-import com.alexstyl.specialdates.people.PeopleViewModelFactory;
-import com.alexstyl.specialdates.permissions.MementoPermissions;
 
 import dagger.Module;
 import dagger.Provides;
@@ -38,18 +36,31 @@ public class PersonModule {
     }
 
     @Provides
-    PersonPresenter presenter(MementoPermissions permissions,
-                              PeopleEventsProvider peopleEventsProvider,
-                              PeopleViewModelFactory viewModelFactory,
-                              CrashAndErrorTracker errorTracker, PersonCallProvider personCallProvider, Strings strings, PeopleEventsPersister peoplePersister, DateLabelCreator dateLabelCreator) {
+    AgeCalculator ageCalculator() {
+        return new AgeCalculator(Date.Companion.today());
+    }
+
+    @Provides
+    PersonDetailsViewModelFactory personDetailsViewModelFactory(Strings strings, AgeCalculator ageCalculator) {
+        return new PersonDetailsViewModelFactory(strings, ageCalculator);
+    }
+
+    @Provides
+    EventViewModelFactory eventViewModelFactory(Strings strings, DateLabelCreator dateLabelCreator) {
+        return new EventViewModelFactory(strings, dateLabelCreator);
+    }
+
+    @Provides
+    PersonPresenter presenter(PeopleEventsProvider peopleEventsProvider,
+                              PersonCallProvider personCallProvider,
+                              PeopleEventsPersister peoplePersister,
+                              PersonDetailsViewModelFactory factory,
+                              EventViewModelFactory toEventViewModel) {
         return new PersonPresenter(
                 peopleEventsProvider,
                 personCallProvider,
-                Schedulers.io(),
-                AndroidSchedulers.mainThread(),
-                new PersonDetailsViewModelFactory(strings, new AgeCalculator(Date.Companion.today())),
-                new EventViewModelFactory(strings, dateLabelCreator),
-                peoplePersister
+                factory, toEventViewModel, peoplePersister, Schedulers.io(),
+                AndroidSchedulers.mainThread()
         );
 
     }
