3df3981cf84dc09a8c225303fa5a8b5f21fbed65
Alex Styl <alexanderstyl@gmail.com>
2018-06-16 02:45:08 +0300

    Add one hour timeframe on the Daily Reminder
    The documentation of the Evernote Job Scheduler instruct that daily jobs may not be exact.

--- android_mobile/src/main/java/com/alexstyl/specialdates/dailyreminder/DailyReminderJob.kt
+++ android_mobile/src/main/java/com/alexstyl/specialdates/dailyreminder/DailyReminderJob.kt
@@ -4,13 +4,12 @@ import com.evernote.android.job.DailyJob
 
 class DailyReminderJob(private val presenter: DailyReminderPresenter,
                        private val notifier: DailyReminderNotifier) : DailyJob() {
-    
+
     companion object {
         const val TAG = "Daily_reminder"
     }
 
     override fun onRunDailyJob(params: Params): DailyJobResult {
-
         val view = NotificationDailyReminderView(notifier)
         presenter.startPresentingInto(view)
         presenter.stopPresenting()
