8b92d7a8eec8e1a53a5f62f750ca773fbf690405
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2018-06-18 14:21:50 +0300

    Keep Color.RED as the header color

--- android_mobile/src/main/java/com/alexstyl/specialdates/upcoming/widget/today/UpcomingWidgetPreviewLayout.java
+++ android_mobile/src/main/java/com/alexstyl/specialdates/upcoming/widget/today/UpcomingWidgetPreviewLayout.java
@@ -2,6 +2,7 @@ package com.alexstyl.specialdates.upcoming.widget.today;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.ColorRes;
 import android.support.annotation.StringRes;
 import android.util.AttributeSet;
@@ -32,10 +33,12 @@ public class UpcomingWidgetPreviewLayout extends FrameLayout {
         LayoutInflater.from(context).inflate(R.layout.merge_upcoming_widget_preview, this, true);
 
         this.header = Views.findById(this, R.id.upcoming_widget_header);
+        this.header.setTextColor(Color.RED);
         this.contactNames = Views.findById(this, R.id.upcoming_widget_events_text);
         ImageView avatar = Views.findById(this, R.id.widget_avatar);
         avatar.setImageDrawable(getResources().getDrawable(R.drawable.ic_contact_portrait));
         this.background = Views.findById(this, R.id.upcoming_widget_background_image);
+
 
         if (isInEditMode()) {
             header.setText("This is a Header");
@@ -62,7 +65,6 @@ public class UpcomingWidgetPreviewLayout extends FrameLayout {
 
     private void setTextColor(@ColorRes int textColorRes) {
         int textColor = getResources().getColor(textColorRes);
-        header.setTextColor(textColor);
         contactNames.setTextColor(textColor);
     }
 
