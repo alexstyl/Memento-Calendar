da6072821fff2d1331eb0f264baf66f7b94ca05e
Alex Styl <alexanderstyl@gmail.com>
2018-07-28 16:38:45 +0300

    Remove null values of Namedaycard

--- android_mobile/src/main/java/com/alexstyl/specialdates/search/NamedayCard.kt
+++ android_mobile/src/main/java/com/alexstyl/specialdates/search/NamedayCard.kt
@@ -2,30 +2,31 @@ package com.alexstyl.specialdates.search
 
 import com.alexstyl.specialdates.date.Date
 import com.alexstyl.specialdates.events.namedays.NameCelebrations
+import com.alexstyl.specialdates.events.namedays.NoNameCelebrations
 
 class NamedayCard {
 
-    private var namedays: NameCelebrations? = null
+    private var namedays: NameCelebrations = NoNameCelebrations("")
 
     val isAvailable: Boolean
-        get() = namedays != null && namedays!!.dates.isNotEmpty()
+        get() = namedays.dates.isNotEmpty()
 
     val name: String
-        get() = namedays!!.name
+        get() = namedays.name
 
     fun setNameday(nameday: NameCelebrations) {
         this.namedays = nameday
     }
 
     fun getDate(i: Int): Date {
-        return namedays!!.dates[i]
+        return namedays.dates[i]
     }
 
     fun clear() {
-        this.namedays = null
+        this.namedays = NoNameCelebrations("")
     }
 
     fun size(): Int {
-        return namedays!!.dates.size
+        return namedays.dates.size
     }
 }
