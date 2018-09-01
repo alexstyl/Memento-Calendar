93ee7942a59c451a35d3a782f4a18f5e46976e58
Daniele Conti <dnlcnt@gmail.com>
2018-09-01 18:00:18 +0200

    Fix a bug for labels of a birthday calculated after the birth day in the current year

--- memento/src/main/java/com/alexstyl/specialdates/date/ContactEvent.kt
+++ memento/src/main/java/com/alexstyl/specialdates/date/ContactEvent.kt
@@ -9,16 +9,22 @@ import com.alexstyl.specialdates.events.peopleevents.StandardEventType
 data class ContactEvent(val deviceEventId: Optional<Long>, val type: EventType, val date: Date, val contact: Contact) {
 
     fun getLabel(dateWithYear: Date, strings: Strings): String {
-        if (type === StandardEventType.BIRTHDAY) {
-            if (date.hasYear()) {
-
-                val age = dateWithYear.year!! - date.year!!
-                if (age > 0) {
-                    return strings.turnsAge(age)
+        if (type === StandardEventType.BIRTHDAY && date.hasYear()) {
+            val age = (dateWithYear.year!! - date.year!!).let {
+                if (birthdayIsBeforeDateIgnoringYear(dateWithYear)) {
+                    it + 1
+                } else {
+                    it
                 }
+            }
+
+            if (age > 0) {
+                return strings.turnsAge(age)
             }
         }
         return type.getEventName(strings)
     }
 
+    private fun birthdayIsBeforeDateIgnoringYear(dateWithYear: Date) =
+        dateWithYear.withoutYear() > date.withoutYear()
 }
